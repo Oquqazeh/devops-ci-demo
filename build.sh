b3a6b4b11d3f55e87f348fe9888592314f98bb45
Oquqazeh <62776494+Oquqazeh@users.noreply.github.com>
2020-04-17 11:24:29 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-echo "Running the build script ..."
-echo "I am $HOSTNAME"
-echo "The time is : $(date %s)."
-echo "The logged in user is : ${USER}."
-echo "Current working directory : ${PWD}"
+
+echo "omar quqazeh"
